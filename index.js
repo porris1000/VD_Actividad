7f3c88aeab9bdf2de569f1626dbc4a223ce8437f
porris1000 <porris1000@gmail.com>
2022-11-16 10:54:56 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,33 +2,36 @@
 import './style.css';
 
 var data1 = [
-  { name: 'index', country: 'India', index: 0.629 },
-  { name: 'index', country: 'Arabia Saudita', index: 0.636 },
-  { name: 'index', country: 'Turquía', index: 0.639 },
-  { name: 'index', country: 'Japón', index: 0.65 },
-  { name: 'index', country: 'China', index: 0.682 },
-  { name: 'index', country: 'Corea del Sur', index: 0.689 },
-  { name: 'index', country: 'Brasil', index: 0.696 },
-  { name: 'index', country: 'Indonesia', index: 0.697 },
-  { name: 'index', country: 'Italia', index: 0.72 },
-  { name: 'index', country: 'Australia', index: 0.738 },
-  { name: 'index', country: 'Argentina', index: 0.756 },
-  { name: 'index', country: 'México', index: 0.764 },
-  { name: 'index', country: 'Estados Unidos', index: 0.769 },
-  { name: 'index', country: 'Canadá', index: 0.772 },
-  { name: 'index', country: 'Reino Unido', index: 0.78 },
-  { name: 'index', country: 'Sudáfrica', index: 0.782 },
-  { name: 'index', country: 'España', index: 0.788 },
-  { name: 'index', country: 'Francia', index: 0.791 },
-  { name: 'index', country: 'Alemania', index: 0.801 },
+  { name: 'index', country: 'India', index: 0.629, group: 4 },
+  { name: 'index', country: 'Arabia Saudita', index: 0.636, group: 0 },
+  { name: 'index', country: 'Turquía', index: 0.639, group: 0 },
+  { name: 'index', country: 'Japón', index: 0.65, group: 0 },
+  { name: 'index', country: 'China', index: 0.682, group: 1 },
+  { name: 'index', country: 'Corea del Sur', index: 0.689, group: 1 },
+  { name: 'index', country: 'Brasil', index: 0.696, group: 1 },
+  { name: 'index', country: 'Indonesia', index: 0.697, group: 1 },
+  { name: 'index', country: 'Italia', index: 0.72, group: 2 },
+  { name: 'index', country: 'Australia', index: 0.738, group: 2 },
+  { name: 'index', country: 'Argentina', index: 0.756, group: 3 },
+  { name: 'index', country: 'México', index: 0.764, group: 3 },
+  { name: 'index', country: 'Estados Unidos', index: 0.769, group: 3 },
+  { name: 'index', country: 'Canadá', index: 0.772, group: 3 },
+  { name: 'index', country: 'Reino Unido', index: 0.78, group: 3 },
+  { name: 'index', country: 'Sudáfrica', index: 0.782, group: 3 },
+  { name: 'index', country: 'España', index: 0.788, group: 3 },
+  { name: 'index', country: 'Francia', index: 0.791, group: 3 },
+  { name: 'index', country: 'Alemania', index: 0.801, group: 3 },
 ];
 
 var visualization = d3plus
   .viz()
   .container('#viz1')
   .data(data1)
+  .sort('index')
   .type('bar')
-  .id('name')
-  .x('country')
+  .id('country')
+  .x('index')
   .y('index')
+  .color('index')
+  .xaxis('country')
   .draw();
